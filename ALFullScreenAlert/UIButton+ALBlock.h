b63d2eb5d3d1f7de1cc188505627afcfbc687c41
Andrea Mario Lufino <andrea.lufino@me.com>
2013-07-15 14:37:23 +0200

    Stable 1.0 version with Demo app

---
+++ ALFullScreenAlert/UIButton+ALBlock.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+ALBlock.h
+//  ALFullScreenAlert
+//
+//  Created by Andrea Mario Lufino on 04/07/13.
+//  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (ALBlock)
+
+@end
---
+++ ALFullScreenAlert/UIButton+ALBlock.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+ALBlock.m
+//  ALFullScreenAlert
+//
+//  Created by Andrea Mario Lufino on 04/07/13.
+//  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
+//
+
+#import "UIButton+ALBlock.h"
+
+@implementation UIButton (ALBlock)
+
+@end
